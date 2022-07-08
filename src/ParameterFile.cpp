468faf392fd36307eb73b2353c65c09e04549953
Stefan Lepperdinger <lepperdinger.stefan@gmail.com>
2022-07-08 21:57:52 +0200

    Added ParameterFile.cpp/.h.

---
+++ src/ParameterFile.cpp
@@ -0,0 +1,75 @@
+#include "ParameterFile.h"
+#include <iostream>
+#include <regex>
+
+#define DEGREES_TO_RADIAN 0.017453292519943295
+
+ParameterFile::ParameterFile(const std::string &file_path)
+    : file_path(file_path) {}
+
+std::string ParameterFile::get_string(const std::string &parameter_name) {
+  std::string line;
+  std::ifstream file(file_path);
+  std::ostringstream regular_expression;
+  regular_expression << "^" << parameter_name << " *= *(.*)$";
+  std::regex regex(regular_expression.str());
+  std::cmatch match;
+  while (std::getline(file, line)) {
+    bool found_line = std::regex_match(line.c_str(), match, regex);
+    if (found_line) {
+      return match[1];
+    }
+  }
+  std::cerr << "error: Couldn't find the parameter '" << parameter_name
+            << "' of the parameter file '" << file_path << "'.\n";
+  std::exit(1);
+}
+
+int ParameterFile::get_int(const std::string &parameter_name) {
+  auto parameter_string = get_string(parameter_name);
+  int parameter;
+  try {
+    parameter = std::stoi(parameter_string);
+  } catch (std::invalid_argument &invalid_argument) {
+    std::cerr << "error: Parsing of the parameter '" << parameter_name
+              << "' of the parameter file '" << file_path << "' failed.\n";
+    std::exit(1);
+  }
+  return parameter;
+}
+
+double ParameterFile::get_double(const std::string &parameter_name) {
+  auto parameter_string = get_string(parameter_name);
+  double parameter;
+  try {
+    parameter = std::stod(parameter_string);
+  } catch (std::invalid_argument &invalid_argument) {
+    std::cerr << "error: Parsing of the parameter '" << parameter_name
+              << "' of the parameter file '" << file_path << "' failed.\n";
+    std::exit(1);
+  }
+  return parameter;
+}
+
+ParameterFile::Parameters ParameterFile::get_parameters() {
+  Parameters parameters;
+  parameters.xyz_observer_location = {
+      get_double("x_observer_location_in_kpc"),
+      get_double("y_observer_location_in_kpc"),
+      get_double("z_observer_location_in_kpc"),
+  };
+  parameters.radial_step_size = get_double("radial_bin_size_in_kpc");
+  parameters.longitudinal_grid_interval = {
+      get_double("longitudinal_grid_minimum_in_degrees") * DEGREES_TO_RADIAN,
+      get_double("longitudinal_grid_maximum_in_degrees") * DEGREES_TO_RADIAN,
+  };
+  parameters.number_of_longitudinal_grid_points =
+      get_int("number_of_longitudinal_grid_points");
+  parameters.latitudinal_grid_interval = {
+      get_double("latitudinal_grid_minimum_in_degrees") * DEGREES_TO_RADIAN,
+      get_double("latitudinal_grid_maximum_in_degrees") * DEGREES_TO_RADIAN,
+  };
+  parameters.number_of_latitudinal_grid_points =
+      get_int("number_of_latitudinal_grid_points");
+  return parameters;
+}
---
+++ src/ParameterFile.h
@@ -0,0 +1,32 @@
+#ifndef GAMMA_SKY_SRC_PARAMETERFILE_H
+#define GAMMA_SKY_SRC_PARAMETERFILE_H
+
+#include <array>
+#include <fstream>
+#include <string>
+
+class ParameterFile {
+public:
+  struct Parameters {
+    // {x, y, z} observer location in kpc
+    std::array<double, 3> xyz_observer_location;
+    // size of the radial bins in kpc
+    double radial_step_size;
+    // minimum and maximum of the longitudinal grid in radian
+    std::array<double, 2> longitudinal_grid_interval;
+    int number_of_longitudinal_grid_points;
+    // minimum and maximum of the latitudinal grid in radian
+    std::array<double, 2> latitudinal_grid_interval;
+    int number_of_latitudinal_grid_points;
+  };
+  explicit ParameterFile(const std::string &file_path);
+  Parameters get_parameters();
+
+private:
+  std::string get_string(const std::string &parameter_name);
+  int get_int(const std::string &parameter_name);
+  double get_double(const std::string &parameter_name);
+  std::string file_path;
+};
+
+#endif // GAMMA_SKY_SRC_PARAMETERFILE_H
