14d67cee0362e705f85542aa2196de905d605c9f
Stefan Lepperdinger <lepperdinger.stefan@gmail.com>
2022-07-09 05:25:14 +0200

    Added the integration intervals to grid.h.

--- src/grids.h
+++ src/grids.h
@@ -1,6 +1,7 @@
 #ifndef GAMMA_SKY_SRC_GRIDS_H
 #define GAMMA_SKY_SRC_GRIDS_H
 
+#include <array>
 #include <vector>
 
 namespace grids {
@@ -19,9 +20,11 @@ struct cartesian_grid_3d {
 struct longitude_latitude_grid {
   std::vector<double> longitude_centers;
   std::vector<double> longitude_boundaries;
+  std::vector<std::array<double, 2>> longitude_integration_intervals;
 
   std::vector<double> latitude_centers;
   std::vector<double> latitude_boundaries;
+  std::vector<std::array<double, 2>> latitude_integration_intervals;
 };
 
 } // namespace grids
